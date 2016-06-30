f6d670d1f967b1045987b7c4e6c1225b064e4d84
Oliver Sanford <oliver.sanford@outlook.com>
2016-06-30 15:45:58 +0100

    initial state for testing

---
+++ mod_tests.py
@@ -0,0 +1,9 @@
+# This is for you to fill in!
+
+from mean import *
+
+def test_mean_1234():
+    num_list = [1,2,3,4]
+    calc_mean = mean(num_list)
+    expected_mean=3
+    assert expected_mean == calc_mean
